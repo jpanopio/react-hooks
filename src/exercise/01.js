d0375d6a4c494e0022e500f0233acec51576e7df
jcpanopio <jcpanopio1@gmail.com>
2020-12-25 14:30:02 -0800

    Do useState and useEffect exercises

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -1,14 +1,15 @@
 // useState: greeting
 // http://localhost:3000/isolated/exercise/01.js
 
-import React from 'react'
+import React, { useState } from 'react'
 
-function Greeting() {
-  // 💣 delete this variable declaration and replace it with a React.useState call
-  const name = ''
+function Greeting(props) {
+  const { initialName = '' } = props;
+  const [name, setName] = useState(initialName);
 
   function handleChange(event) {
     // 🐨 update the name here based on event.target.value
+    setName(event.target.value);
   }
 
   return (
@@ -23,7 +24,7 @@ function Greeting() {
 }
 
 function App() {
-  return <Greeting />
+  return <Greeting initialName="Larry" />
 }
 
 export default App
--- src/exercise/02.js
+++ src/exercise/02.js
@@ -3,14 +3,27 @@
 
 import React from 'react'
 
+const useLocalStorageState = (key, value) => {
+  const [storedValue, setStoredValue] = React.useState(() => window.localStorage.getItem(key) || value);
+  
+  React.useEffect(() => {
+    window.localStorage.setItem(key, storedValue);
+  }, [storedValue]);
+
+  return [storedValue, setStoredValue];
+};
+
 function Greeting({initialName = ''}) {
   // 🐨 initialize the state to the value from localStorage
   // 💰 window.localStorage.getItem('name') || initialName
-  const [name, setName] = React.useState(initialName)
-
+  // const [name, setName] = React.useState(() => window.localStorage.getItem('name') || initialName)
+  const [name, setName] = useLocalStorageState('name', initialName);
   // 🐨 Here's where you'll use `React.useEffect`.
   // The callback should set the `name` in localStorage.
   // 💰 window.localStorage.setItem('name', name)
+  // React.useEffect(() => {
+  //   window.localStorage.setItem('name', name);
+  // }, [name]);
 
   function handleChange(event) {
     setName(event.target.value)
